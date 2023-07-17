28483544460ec89416d8ef5173a49f25be293299
haejun <hj645000@gmail.com>
2023-07-17 09:27:54 +0900

    2032/07/17

--- 4주차/simulation/app.py
+++ 4주차/simulation/app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def logo():
     return render_template('logo.html')
 
+@app.route('/ocr')
+def ocr():
+    return render_template('ocr.html')
+
 @app.route('/index')
 def index():
     return render_template('index.html')
